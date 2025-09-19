22a80e83bb3de12fc5c5d9b796802ec9fb8c8799
MAHFUZA AKTER <68926349+mahfuzaakter00@users.noreply.github.com>
2025-09-19 09:34:10 -0700

    Technical Documentation Page project
    Initial upload of Technical Documentation Page project for Responsive Web Design certificate.
    (Includes project instructions, HTML, CSS, and JavaScript for the Technical Documentation Page).

---
+++ Technical Documentation Page/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#theme-toggle").click(function(){
+        $("body").toggleClass("darkMode").css({
+            "transition": "background-color 1s, color 1s"
+        });
+        
+
+        if($(this).children(".mode-icon").hasClass("far fa-lightbulb")){
+            $(this).children(".mode-icon").removeClass("far fa-lightbulb").addClass("fas fa-lightbulb").addClass("pulse-glow");
+            setTimeout(()=>$(this).children(".mode-icon").removeClass("pulse-glow"),500);
+        }
+        else{
+            $(this).children(".mode-icon").removeClass("fas fa-lightbulb").addClass("far fa-lightbulb"); 
+        
+        }
+        
+    });
+
+    // jQuery scroll-spy to highlight nav-links while scrolling the related sections
+
+    $(window).on("scroll", function () {
+        $("section").each(function () {
+            if (
+                $(window).scrollTop() >= $(this).offset().top &&
+                $(window).scrollTop() <= $(this).offset().top + $(this).outerHeight()
+            ) {
+                $(".nav-link").removeClass("active");
+                $(".nav-link[href='#" + $(this).attr("id") + "']").addClass("active");
+            }
+        });
+    });
+
+
+});
