c63fbb80270621470bd8b0c2570e70065d904ede
MAHFUZA AKTER <68926349+mahfuzaakter00@users.noreply.github.com>
2025-10-12 08:10:00 -0700

    Upload Survey Form
    Initial upload of Survey Form Project for Responsive Web Design Certificate.

---
+++ Survey Form/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#theme-toggle").click(function(){
+        $("body").toggleClass("darkMode").css({
+            "transition": "background-color 1s, color 1s"
+        });
+        
+        $("input").toggleClass("darkMode");
+        $("textarea").toggleClass("darkMode");
+        $("select").toggleClass("darkMode");
+
+        if($(this).children(".mode-icon").hasClass("fas fa-lightbulb")){
+            $(this).children(".mode-icon").removeClass("fas fa-lightbulb").addClass("far fa-lightbulb").addClass("pulse-glow");
+            setTimeout(()=>$(this).children(".mode-icon").removeClass("pulse-glow"),500);
+        }
+        else{
+            $(this).children(".mode-icon").removeClass("far fa-lightbulb").addClass("fas fa-lightbulb"); 
+        
+        }
+        
+    });
+
+});
